c34b3113c0a10c8d3f2031ffe96cf4cdf5acb22c
Matthew Lewis <matthew.lewis@mulesoft.com>
2020-10-07 10:06:51 +0100

    mvn exec plugin, changed LogTest to Main

--- src/main/java/test/Main.java
+++ src/main/java/test/Main.java
@@ -5,7 +5,7 @@ import org.tinylog.Logger;
 import test.a.A;
 import test.b.B;
 
-public class LogTest {
+public class Main {
 	public static void main(String[] args) {
 		Logger.info("in main()");
 		Logger.debug("in main()");
